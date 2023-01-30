3985be97021dd4915c1c9619f31b1f6af5a9d939
Birleyici <davudtevhid@gmail.com>
2023-01-30 23:13:45 +0300

    klj

--- components/SignUpHead.vue
+++ components/SignUpHead.vue
@@ -5,7 +5,7 @@
           <div class="d-flex flex-row justify-content-center">
               <div class="row">
                   <div class="col-md">
-                      <img class="img_1" src="logo.png" alt="Amazon Logo" />
+                      <img class="img_1" src="/logo.png" alt="Amazon Logo" />
                   </div>
               </div>
           </div>
